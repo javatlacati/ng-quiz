dc755760c0368f94c183ecc891fb8e15c3d3f0ac
javatlacati <scherzo_16@hotmail.com>
2021-10-10 10:10:42 -0500

    migrating feedback component

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Question from "../../model/Question";
+import MultipleChoiceQuestion from "../../model/MultipleChoiceQuestion";
+import {PageEvent} from "@angular/material/paginator";
+import {Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {QuestionSubscription} from "../../subscriptions/QuestionSubscription";
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +13,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+  questionsData: Question[] = []
+  preguntaActual = 1
+  selectedAnswer: { value: number, label: string } | null = null
+  private subscription: Subscription | null = null;
+
+  constructor(private questionSuubscription: QuestionSubscription) {
+  }
 
   ngOnInit(): void {
+    this.subscription = this.questionSuubscription
+      .currentSharedQuestions
+      .subscribe((theQuestions: Question[]) => {
+        if (theQuestions.length > 0) {
+          // this.laPreguntaActual = theQuestions[0];
+        }
+        return this.questionsData = theQuestions;
+      });
+
   }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  public handleQuestionChange(event?: PageEvent) {
+    this.preguntaActual = event?.pageIndex || 0;
+    // this.laPreguntaActual = this.questionsData[this.preguntaActual];
+  }
+
+  // questionChanged() {
+  //   let daQuestion: Question; //= this.$route.params.questions[this.preguntaActual - 1] as unknown as Question;
+  //   let savedAnswer = daQuestion.userAnswer;
+  //   console.log(`saved user answer: ${savedAnswer}`)
+  //   if (savedAnswer === '') {
+  //     this.selectedAnswer = null
+  //   } else {
+  //     let className = daQuestion.constructor.name;
+  //     console.log(`className:${className}`)
+  //     switch (className) {
+  //       case 'MultipleChoiceQuestion':
+  //         let daQuestionElementElement = (daQuestion as MultipleChoiceQuestion).choices[savedAnswer];
+  //         console.log(`saved answer value:${daQuestionElementElement}`)
+  //         console.log(`saved answer value:${daQuestionElementElement}`)
+  //         this.selectedAnswer = {value: ~~savedAnswer, label: daQuestionElementElement}
+  //         break;
+  //     }
+  //   }
+  // }
 
 }
