1286fa831f134769c125bf1032d7c301f38dba39
javatlacati <scherzo_16@hotmail.com>
2021-10-17 18:40:43 -0500

    adding questionset selection at the beginning

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,12 +1,13 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import DatasetLoader from "../../model/DatasetLoader";
 import Question from "../../model/Question";
 import {HttpClient} from "@angular/common/http";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {QuestionSubscription} from "../../subscriptions/QuestionSubscription";
-import {Subscription} from "rxjs";
 import {StepperSelectionEvent} from "@angular/cdk/stepper";
+import {QuestionDatasetEntry} from "../../model/QuestionDatasetEntry";
+import FillBlankQuestion from "../../model/FillBlankQuestion";
 
 @Component({
   selector: 'app-homepage',
@@ -15,15 +16,18 @@ import {StepperSelectionEvent} from "@angular/cdk/stepper";
 })
 export class HomepageComponent implements OnInit {
 
-  questions: Question[] = new DatasetLoader(this.httpClient).createQuestionsFromFile();
+  questions: Question[] = [];//new DatasetLoader(this.httpClient).createQuestionsFromFile();
   asssitantStep: number = 1;
   maxQuestions = -1;
+  questionSet: QuestionDatasetEntry[] = [{displayValue: 'vue', filename: 'vue.txt'}];
   categorySelection: string[] = [];
+  categories: string[] = [];
   difficultySelection: string[] = [];
   questionDifficulties: string[] = ['Easy', 'Normal', 'Hard'];
   errorMessage = ''
   alert: boolean = false
 
+  zerothFormGroup!: FormGroup;
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
   thirdFormGroup!: FormGroup;
@@ -37,7 +41,12 @@ export class HomepageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // leer de assets nombres de archivos
 
+
+    this.zerothFormGroup = this._formBuilder.group({
+      zerothCtrl: ['', Validators.required]
+    });
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
@@ -47,13 +56,6 @@ export class HomepageComponent implements OnInit {
     this.thirdFormGroup = this._formBuilder.group({
       thirdCtrl: ['', Validators.required]
     });
-  }
-
-  // computed
-  get questionCategories(): string[] {
-    const categories = [...new Set(this.questions.map(question => question.category))]
-    // console.log(JSON.stringify(categories))
-    return categories
   }
 
   goToQuiz() {
@@ -66,6 +68,7 @@ export class HomepageComponent implements OnInit {
         questionsToBePassed = questionsToBePassed.slice(0, this.maxQuestions)
       }
       this.questionSuubscription.updateSharedQuestions(questionsToBePassed);
+      console.log('questionsToBePassed:', questionsToBePassed)
       this.router.navigate(['/quiz'])
     }
   }
@@ -73,9 +76,23 @@ export class HomepageComponent implements OnInit {
   stepClick(evt: StepperSelectionEvent) {
     let label = evt.selectedStep.label;
     console.log(label)
-    console.log('category: ', this.categorySelection)
     if (label === 'step3') {
+      console.log('category selection: ', this.categorySelection)
       this.questions = this.questions.filter(question => this.categorySelection.includes(question.category))
+    } else if (label === 'step1') {
+
+      for (const questionSetSelection of this.questionSet) {
+        let questions = new DatasetLoader(this.httpClient).createQuestionsFromFile(questionSetSelection.filename);
+        let questionsb = questions;
+        console.log('las preguntas', JSON.stringify(questions))
+        console.log('las preguntas', questions)
+        questionsb.push(...this.questions);
+        this.questions = questionsb;
+
+      }
+      console.log('this.questions:', JSON.stringify(this.questions));
+      this.categories = [...new Set(this.questions.map(question => question.category))]
+      console.log('this.categories:', this.categories);
     }
   }
 }
--- src/app/model/DatasetLoader.ts
+++ src/app/model/DatasetLoader.ts
@@ -7,147 +7,145 @@ import {HttpClient} from "@angular/common/http";
 
 export default class DatasetLoader {
   private httpClient: HttpClient;
-    questions: Question[] = []
+  questions: Question[] = []
 
 
   constructor(httpClient: HttpClient) {
     this.httpClient = httpClient;
   }
 
-  createQuestionsFromFile(): Question[] {
-    this.httpClient.get('assets/vue.txt', { responseType: 'text' })
-      .subscribe(quizText => {
-        if (quizText) {
-          let lines = quizText.split(/\r\n/g);
-          // console.log(`loaded lines:` + lines.length)
-          lines.forEach(line => this.parseQuestion(line));
-        }
-      });
-        //const quizText: { default: string } = require('../assets/vue.txt');
+  createQuestionsFromFile(fileName: string): Question[] {
+    this.httpClient.get('assets/' + fileName, {responseType: 'text'})
+      .toPromise().then(quizText => {
+      if (quizText) {
+        let lines = quizText.split(/\r\n/g);
+        // console.log(`loaded lines:` + lines.length)
+        lines.forEach(line => this.parseQuestion(line));
+      }
+    });
+    return this.questions;
+  }
 
-        return this.questions;
+  parseQuestion(line: string) {
+    let questarray: string[] = line.split(/@@/g);
+    // console.log(`questarray:` + questarray.length)
+    // console.log(JSON.stringify(questarray))
+    let tipoPregunta: string = questarray[0];
+    let vettedness: string = "v" === questarray[1] ? Question.VETTED : Question.TRIAL;
+    let explanation: string = questarray[2];
+    let category: string = questarray[3];
+    let difficulty: string = questarray[4];
+    // console.log(questarray[5])
+    let questionText: string = questarray[5];//this.formateaPregunta(questarray[5]);
+    // console.log(questionText)
+    switch (tipoPregunta) {
+      case "MC": {
+        //multiple choice
+        let correctAnswerIdx = ~~questarray[6];
+        let answersTexts = questarray.slice(7, questarray.length);
+        this.addMultipleChoiceQuestion(this.questions, vettedness, explanation,
+          questionText, correctAnswerIdx, category, difficulty, answersTexts);
+        break;
+      }
+      case "FB": {
+        //fill in the blanks
+        let blanks = questarray.slice(6, questarray.length);
+        console.log(`blanks: ${blanks}`);
+        this.addFillBlankQuestion(this.questions, vettedness, explanation, questionText, category, difficulty, blanks);
+        break;
+      }
+      case "MA": {
+        let choices: Map<string, boolean> = this.parseChoicesMap(questarray.slice(6, questarray.length));
+        this.addMultipleAnswerQuestion(this.questions, vettedness, explanation, questionText,
+          category, difficulty, choices);
+        break;
+      }
+      case "":
+        break;
+      default:
+        console.error(`Question type not recognized: ${tipoPregunta}`)
+        break;
     }
+    // console.log(JSON.stringify(this.questions));
+  }
 
-    parseQuestion(line: string) {
-        let questarray: string[] = line.split(/@@/g);
-        // console.log(`questarray:` + questarray.length)
-        // console.log(JSON.stringify(questarray))
-        let tipoPregunta: string = questarray[0];
-        let vettedness: string = "v" === questarray[1] ? Question.VETTED : Question.TRIAL;
-        let explanation: string = questarray[2];
-        let category: string = questarray[3];
-        let difficulty: string = questarray[4];
-        // console.log(questarray[5])
-        let questionText: string = questarray[5];//this.formateaPregunta(questarray[5]);
-        // console.log(questionText)
-        switch (tipoPregunta) {
-            case "MC": {
-                //multiple choice
-                let correctAnswerIdx = ~~questarray[6];
-                let answersTexts = questarray.slice(7, questarray.length);
-                this.addMultipleChoiceQuestion(this.questions, vettedness, explanation,
-                    questionText, correctAnswerIdx, category, difficulty, answersTexts);
-                break;
-            }
-            case "FB": {
-                //fill in the blanks
-                let blanks = questarray.slice(6, questarray.length);
-                console.log(`blanks: ${blanks}`);
-                this.addFillBlankQuestion(this.questions, vettedness, explanation, questionText, category, difficulty, blanks);
-                break;
-            }
-            case "MA": {
-                let choices: Map<string, boolean> = this.parseChoicesMap(questarray.slice(6, questarray.length));
-                this.addMultipleAnswerQuestion(this.questions, vettedness, explanation, questionText,
-                    category, difficulty, choices);
-                break;
-            }
-            case "":
-                break;
-            default:
-                console.error(`Question type not recognized: ${tipoPregunta}`)
-                break;
-        }
-        // console.log(JSON.stringify(this.questions));
-    }
+  addMultipleAnswerQuestion(questions: Question[], vettedness: string, explanation: string, questionText: string, category: string, difficulty: string, answerChoicesMap: Map<string, boolean>) {
+    let choiceQuestion = new MultipleAnswerQuestion(vettedness);
+    choiceQuestion.explanation = explanation;
+    choiceQuestion.text = questionText;
+    choiceQuestion.category = category;
+    choiceQuestion.difficulty = this.str2difficulty(difficulty);
+    // answerChoicesMap.forEach((choice, i) => choiceQuestion.setChoice(choice, i));
+    choiceQuestion.choices = Array.from(answerChoicesMap.keys());
+    choiceQuestion.answer = Array.from(answerChoicesMap.values()).map((value: boolean, idx: number) => {
+      if (value) {
+        return idx;
+      } else {
+        return -1;
+      }
+    }).filter((idxs): boolean => idxs > 0).join(' ');
+    choiceQuestion.correctAnswers = Array.from(Array.from(answerChoicesMap.values()).map((value, idx) => {
+      if (value) {
+        return idx;
+      } else {
+        return -1;
+      }
+    }), (theValue) => `${theValue}`).filter((str) => str !== 'undefined');
+    questions.push(choiceQuestion);
+  }
 
-    addMultipleAnswerQuestion(questions: Question[], vettedness: string, explanation: string, questionText: string, category: string, difficulty: string, answerChoicesMap: Map<string, boolean>) {
-        let choiceQuestion = new MultipleAnswerQuestion(vettedness);
-        choiceQuestion.explanation = explanation;
-        choiceQuestion.text = questionText;
-        choiceQuestion.category = category;
-        choiceQuestion.difficulty = this.str2difficulty(difficulty);
-        // answerChoicesMap.forEach((choice, i) => choiceQuestion.setChoice(choice, i));
-        choiceQuestion.choices = Array.from(answerChoicesMap.keys());
-        choiceQuestion.answer = Array.from(answerChoicesMap.values()).map((value: boolean, idx: number) => {
-            if (value) {
-                return idx;
-            } else {
-                return -1;
-            }
-        }).filter((idxs): boolean => idxs > 0).join(' ');
-        choiceQuestion.correctAnswers = Array.from(Array.from(answerChoicesMap.values()).map((value, idx) => {
-            if (value) {
-                return idx;
-            } else {
-               return -1;
-            }
-        }), (theValue) => `${theValue}`).filter((str) => str !== 'undefined');
-        questions.push(choiceQuestion);
+  str2difficulty(idxStr: string) {
+    switch (idxStr) {
+      case "2":
+        return Difficulty.EASY;
+      case "1":
+        return Difficulty.HARD;
+      case "0":
+      default:
+        return Difficulty.NORMAL;
     }
+  }
 
-    str2difficulty(idxStr: string) {
-        switch (idxStr) {
-            case "2":
-                return Difficulty.EASY;
-            case "1":
-                return Difficulty.HARD;
-            case "0":
-            default:
-                return Difficulty.NORMAL;
-        }
-    }
+  parseChoicesMap(questionMapArr: string[]): Map<string, boolean> {
+    let questarray: string[] = questionMapArr.slice(0, questionMapArr.length / 2);
+    let strAnswerValidities: string[] = questionMapArr.slice(questionMapArr.length / 2);
+    let answerValidities: boolean[] = strAnswerValidities.map(
+      (booleanAnswer) => "true".toUpperCase() === booleanAnswer);
+    return this.createAnswerChoicesMap(questarray, answerValidities);
+  }
 
-    parseChoicesMap(questionMapArr: string[]): Map<string, boolean> {
-        let questarray: string[] = questionMapArr.slice(0, questionMapArr.length / 2);
-        let strAnswerValidities: string[] = questionMapArr.slice(questionMapArr.length / 2);
-        let answerValidities: boolean[] = strAnswerValidities.map(
-            (booleanAnswer) => "true".toUpperCase() === booleanAnswer);
-        return this.createAnswerChoicesMap(questarray, answerValidities);
+  createAnswerChoicesMap(answerTexts: string[], answerValidities: boolean[]): Map<string, boolean> {
+    let numsOfAnswers = answerTexts.length;
+    if (numsOfAnswers !== answerValidities.length) {
+      throw new Error("Answer texts should match answer values one by one");
     }
+    let hashMap = new Map<string, boolean>();
+    for (let answIdx = 0; answIdx < answerTexts.length; answIdx++) {
+      hashMap.set(answerTexts[answIdx], answerValidities[answIdx]);
+    }
+    return hashMap;
+  }
 
-    createAnswerChoicesMap(answerTexts: string[], answerValidities: boolean[]): Map<string, boolean> {
-        let numsOfAnswers = answerTexts.length;
-        if (numsOfAnswers !== answerValidities.length) {
-            throw new Error("Answer texts should match answer values one by one");
-        }
-        let hashMap = new Map<string, boolean>();
-        for (let answIdx = 0; answIdx < answerTexts.length; answIdx++) {
-            hashMap.set(answerTexts[answIdx], answerValidities[answIdx]);
-        }
-        return hashMap;
-    }
+  addFillBlankQuestion(questions: Question[], vettedness: string, explanation: string, questionText: string, category: string, difficulty: string, blanks: string[]) {
+    let fillBlankQuestion = new FillBlankQuestion(vettedness);
+    fillBlankQuestion.explanation = explanation;
+    fillBlankQuestion.text = questionText;
+    fillBlankQuestion.category = category;
+    fillBlankQuestion.difficulty = this.str2difficulty(difficulty);
+    fillBlankQuestion.answer = blanks.join(' ');
+    questions.push(fillBlankQuestion);
+  }
 
-    addFillBlankQuestion(questions: Question[], vettedness: string, explanation: string, questionText: string, category: string, difficulty: string, blanks: string[]) {
-        let fillBlankQuestion = new FillBlankQuestion(vettedness);
-        fillBlankQuestion.explanation = explanation;
-        fillBlankQuestion.text = questionText;
-        fillBlankQuestion.category = category;
-        fillBlankQuestion.difficulty = this.str2difficulty(difficulty);
-        fillBlankQuestion.answer = blanks.join(' ');
-        questions.push(fillBlankQuestion);
+  addMultipleChoiceQuestion(questions: Question[], vettedness: string, explanation: string, questionText: string, correctAnswerIdx: number, category: string, difficulty: string, answersTexts: string[]) {
+    let choiceQuestion = new MultipleChoiceQuestion(vettedness);
+    choiceQuestion.explanation = explanation;
+    choiceQuestion.text = questionText;
+    choiceQuestion.category = category;
+    choiceQuestion.difficulty = this.str2difficulty(difficulty);
+    for (let i = 0; i < answersTexts.length; i++) {
+      let answerText = answersTexts[i];
+      choiceQuestion.setChoice(answerText, i == correctAnswerIdx);
     }
-
-    addMultipleChoiceQuestion(questions: Question[], vettedness: string, explanation: string, questionText: string, correctAnswerIdx: number, category: string, difficulty: string, answersTexts: string[]) {
-        let choiceQuestion = new MultipleChoiceQuestion(vettedness);
-        choiceQuestion.explanation = explanation;
-        choiceQuestion.text = questionText;
-        choiceQuestion.category = category;
-        choiceQuestion.difficulty = this.str2difficulty(difficulty);
-        for (let i = 0; i < answersTexts.length; i++) {
-            let answerText = answersTexts[i];
-            choiceQuestion.setChoice(answerText, i == correctAnswerIdx);
-        }
-        questions.push(choiceQuestion);
-    }
+    questions.push(choiceQuestion);
+  }
 }
