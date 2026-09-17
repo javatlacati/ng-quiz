a0fb84a0c574fe8e85f10123e6f1788566edf2d3
Javatlacati <scherzo_16@hotmail.com>
2026-09-16 18:47:00 -0600

    avance en señales para componente de generación de pregunta

--- src/app/components/questiongen/questiongen.component.ts
+++ src/app/components/questiongen/questiongen.component.ts
@@ -1,30 +1,30 @@
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, signal, computed} from '@angular/core';
 import Question from "../../model/Question";
 import FillBlankQuestion from "../../model/FillBlankQuestion";
 import MultipleAnswerQuestion from "../../model/MultipleAnswerQuestion";
 import MultipleChoiceQuestion from "../../model/MultipleChoiceQuestion";
 import Difficulty from "../../model/Difficulty";
-import {OneExampleQuestion} from "../../model/OneExampleQuestion";
 import {GenerationStrategyChooser} from "../../business/GenerationStrategyChooser";
 import {Optional} from "typescript-optional";
 import {MatSnackBar} from "@angular/material/snack-bar";
-import { MatCard, MatCardContent } from '@angular/material/card';
-import { MatSelect, MatOption } from '@angular/material/select';
-import { MatFormField } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatIconButton, MatButton } from '@angular/material/button';
-import { MatTooltip } from '@angular/material/tooltip';
-import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
-import { MatIcon } from '@angular/material/icon';
-import { MatCheckbox } from '@angular/material/checkbox';
+import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatSelect, MatOption} from '@angular/material/select';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {MatIconButton, MatButton} from '@angular/material/button';
+import {MatTooltip} from '@angular/material/tooltip';
+import {CdkCopyToClipboard} from '@angular/cdk/clipboard';
+import {MatIcon} from '@angular/material/icon';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {form, FormField} from "@angular/forms/signals";
 
 @Component({
-    selector: 'app-questiongen',
-    templateUrl: './questiongen.component.html',
-    styleUrls: ['./questiongen.component.scss'],
-    changeDetection: ChangeDetectionStrategy.Eager,
-    imports: [MatCard, MatCardContent, MatSelect, MatOption, MatFormField, MatInput, ReactiveFormsModule, FormsModule, MatIconButton, MatTooltip, CdkCopyToClipboard, MatIcon, MatButton, MatCheckbox]
+  selector: 'app-questiongen',
+  templateUrl: './questiongen.component.html',
+  styleUrls: ['./questiongen.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
+  imports: [MatCard, MatCardContent, MatSelect, MatOption, MatFormField, MatInput, ReactiveFormsModule, FormsModule, MatIconButton, MatTooltip, CdkCopyToClipboard, MatIcon, MatButton, MatCheckbox, FormField]
 })
 export class QuestiongenComponent implements OnInit {
   questionTypes = [
@@ -33,15 +33,27 @@ export class QuestiongenComponent implements OnInit {
     , {name: 'Multiple Answer', value: 'MultipleAnswerQuestion'}
     , {name: 'Give an Example Answer', value: 'OneExampleQuestion'}
   ];
-  questionTypeSelection: string = 'FillBlankQuestion';
   questionDifficulties: string[] = ['Easy', 'Normal', 'Hard'];
-  optionToBeAdded: string = '';
-  multipleAnswerCorrect: boolean = false;
-  currentQuestionAlv: Question = new FillBlankQuestion("Yes");
-  correctChoiceIdx: number = 0;
-  currentDifficulty: string = 'Easy';
 
-  currentGenerationStrategy = GenerationStrategyChooser.getStrategy(this.questionTypeSelection)
+  currentQuestionAlv = signal<Question>(new FillBlankQuestion("Yes"));
+
+  questionGenerationModel=signal({
+    questionType: 'FillBlankQuestion',
+    questionText: '',
+    answer: '',
+    explanation: '',
+    correctAnswer: '',
+    wrongAnswers: [],
+    correctChoiceIdx: 0,
+    difficulty: 'Easy',
+    category: '',
+    optionToBeAdded: ''
+  })
+
+  formQuestionGeneration = form(this.questionGenerationModel,(form) => {})
+
+  multipleAnswerCorrect: boolean = false;
+  currentGenerationStrategy = computed(() => GenerationStrategyChooser.getStrategy(this.questionGenerationModel().questionType))
 
   constructor(public _snackBar: MatSnackBar) {
   }
@@ -50,16 +62,14 @@ export class QuestiongenComponent implements OnInit {
   }
 
   changeType(value: string) {
-    this.questionTypeSelection = value;
-    this.currentGenerationStrategy = GenerationStrategyChooser.getStrategy(this.questionTypeSelection)
     console.log('changing question type to:' + value)
+    console.log('current question type:' + this.questionGenerationModel().questionType)
     console.log('previous type:' + this.currentQuestionAlv.constructor.name)
-    Optional.ofNullable(this.currentGenerationStrategy).ifPresent(parseStrategy => {
-      parseStrategy.resetQuestion()
-      this.currentQuestionAlv = parseStrategy.resetQuestion()
+    Optional.ofNullable(this.currentGenerationStrategy()).ifPresent(parseStrategy => {
+      this.currentQuestionAlv.set(parseStrategy.resetQuestion())
     })
 
-    this.optionToBeAdded = ''
+    this.questionGenerationModel.set({...this.questionGenerationModel(), optionToBeAdded: ''})
     console.log('new type:' + this.currentQuestionAlv.constructor.name)
   }
 
@@ -78,21 +88,25 @@ export class QuestiongenComponent implements OnInit {
   }
 
   addAnswerChoice() {
-    if (this.currentQuestionAlv instanceof MultipleAnswerQuestion) {
-      console.log(`answer choices: ${JSON.stringify(this.currentQuestionAlv.choices)}`)
-      this.currentQuestionAlv.setChoice(this.optionToBeAdded, this.multipleAnswerCorrect);
+    if (this.currentQuestionAlv() instanceof MultipleAnswerQuestion) {
+      let questionToAddChoice = this.currentQuestionAlv() as MultipleAnswerQuestion;
+      console.log(`answer choices: ${JSON.stringify(questionToAddChoice.choices)}`)
+      questionToAddChoice.setChoice(this.questionGenerationModel().optionToBeAdded, this.multipleAnswerCorrect);
+      this.currentQuestionAlv.set(questionToAddChoice)
     } else {
-      if (this.currentQuestionAlv instanceof MultipleChoiceQuestion) {
-        console.log(`answer choices: ${JSON.stringify(this.currentQuestionAlv.choices)}`)
-        this.currentQuestionAlv.setChoice(this.optionToBeAdded, this.currentQuestionAlv.choices.length + 1 === this.correctChoiceIdx);
+      if (this.currentQuestionAlv() instanceof MultipleChoiceQuestion) {
+        let questionToAddChoice = this.currentQuestionAlv() as MultipleChoiceQuestion;
+        console.log(`answer choices: ${JSON.stringify(questionToAddChoice.choices)}`)
+        questionToAddChoice.setChoice(this.questionGenerationModel().optionToBeAdded, questionToAddChoice.choices.length + 1 === this.questionGenerationModel().correctChoiceIdx);
+        this.currentQuestionAlv.set(questionToAddChoice)
       }
     }
-    this.optionToBeAdded = '';
+    this.questionGenerationModel.set({...this.questionGenerationModel(), optionToBeAdded: ''})
   }
 
   generateEnunciate(): string {
-    return Optional.ofNullable(this.currentGenerationStrategy)
-      .map(generationStrategy => generationStrategy.generateEnunciate(this.currentQuestionAlv, this.mapDifficulty(this.currentDifficulty).valueOf(), this.correctChoiceIdx))
+    return Optional.ofNullable(this.currentGenerationStrategy())
+      .map(generationStrategy => generationStrategy.generateEnunciate(this.currentQuestionAlv(), this.mapDifficulty(this.questionGenerationModel().difficulty).valueOf(), this.questionGenerationModel().correctChoiceIdx))
       .orElse("");
   }
 
